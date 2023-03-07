80ad200faf17f89c022cd676980b4a5b8dd96961
SpikeHimself <spike@spikehimself.ovh>
2023-03-07 21:31:46 +0100

    Move plugin consts to new generic Mod namespace

---
+++ XStorage/ModInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Mod
+{
+    public static class Info
+    {
+        // This is *the* place to edit plugin details. Everywhere else will be generated based on this info.
+        public const string GUID = "yay.spikehimself.xstorage";
+        public const string HarmonyGUID = GUID + ".harmony";
+        public const string Author = "SpikeHimself";
+        public const string Name = "XStorage";
+        public const string GitHubRepo = "SpikeHimself/XStorage";
+        public const string Version = "1.0.2";
+        public const string Description = "Open multiple chests at once, rename them, and move items/stacks to the most suitable chest.";
+        public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
+        public const int NexusId = 2290;
+        //public const string BepInVersion = ??
+        public const string JotunnVersion = Jotunn.Main.Version;
+    }
+}
