53c42ec4fb14f282cef66bc9657ee6e28dd3c520
SpikeHimself <spike@spikehimself.ovh>
2023-03-09 01:28:31 +0100

    Add config options

---
+++ XStorage/XStorageConfig.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using XStorage.GUI;
+
+namespace XStorage
+{
+    public class XStorageConfig
+    {
+        public const string ZdoProperty_ContainerName = "XStorage_Name";
+        public const string ZdoProperty_GridSize = "XStorage_Size";
+
+        ////////////////////////////
+        //// Singleton instance ////
+        private static readonly Lazy<XStorageConfig> lazy = new Lazy<XStorageConfig>(() => new XStorageConfig());
+        public static XStorageConfig Instance { get { return lazy.Value; } }
+        ////////////////////////////
+
+        private ConfigFile configFile;
+        public ConfigEntry<int> MaxColumns;
+        public ConfigEntry<int> MaxRows;
+        public ConfigEntry<GridSize.ExpandPreference> ExpandPreference;
+
+        public GridSize MaxSize
+        {
+            get
+            {
+                return new GridSize(MaxColumns.Value, MaxRows.Value);
+            }
+        }
+
+        /// <summary>
+        /// Load the config file, and track the settings inside it
+        /// </summary>
+        /// <param name="configFile">The config file being loaded</param>
+        public void LoadLocalConfig(ConfigFile configFile)
+        {
+            this.configFile = configFile;
+
+            // Add Nexus ID to config for Nexus Update Check (https://www.nexusmods.com/valheim/mods/102)
+            configFile.Bind("General", "NexusID", Mod.Info.NexusId, "Nexus mod ID for updates (do not change)");
+
+            MaxColumns = configFile.Bind("UI", "MaxColumns", 2, "The maximum amount of columns XStorage can expand the containers panel to.");
+            MaxRows = configFile.Bind("UI", "MaxRows", 3, "The maximum amount of rows XStorage can expand the containers panel to.");
+            ExpandPreference = configFile.Bind("UI", "ExpandPreference", GridSize.ExpandPreference.ColumnsFirst, "Determines how XStorage expands the containers panel.");
+
+            //this.configFile.ConfigReloaded += LocalConfigChanged;
+            //this.configFile.SettingChanged += LocalConfigChanged;
+        }
+    }
+}
