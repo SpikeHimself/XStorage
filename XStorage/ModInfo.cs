c4ef25fa0e87a51f63152519707b726cf8bb169d
SpikeHimself <spike@spikehimself.ovh>
2023-03-14 01:22:28 +0100

    Update Jotun 2.11.0, BepInEx 5.4.21

--- XStorage/ModInfo.cs
+++ XStorage/ModInfo.cs
@@ -12,7 +12,7 @@
         public const string Description = "Open multiple chests at once, rename them, and move items/stacks to the most suitable chest.";
         public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
         public const int NexusId = 2290;
-        //public const string BepInVersion = ??
+        public const string BepInExPackVersion = "5.4.2100";
         public const string JotunnVersion = Jotunn.Main.Version;
     }
 }
