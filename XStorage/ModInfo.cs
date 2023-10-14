6406e7df8466499e503503534faecb858a8f8edd
SpikeHimself <spike@spikehimself.ovh>
2023-10-14 13:01:15 +0200

    Prepare v1.1.16, Update Jotunn v2.14.4, BepInEx v5.4.2200

--- XStorage/ModInfo.cs
+++ XStorage/ModInfo.cs
@@ -8,11 +8,11 @@
         public const string Author = "SpikeHimself";
         public const string Name = "XStorage";
         public const string GitHubRepo = "SpikeHimself/XStorage";
-        public const string Version = "1.1.5";
+        public const string Version = "1.1.6";
         public const string Description = "Open multiple chests at once, rename them, and move items/stacks to the most suitable chest.";
         public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
         public const int NexusId = 2290;
-        public const string BepInExPackVersion = "5.4.2105";
+        public const string BepInExPackVersion = "5.4.2200";
         public const string JotunnVersion = Jotunn.Main.Version;
     }
 }
